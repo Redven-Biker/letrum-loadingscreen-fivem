f6141e7257ece5a73bd912fe9c13905d03518fb5
Redven-Biker <marween35620@gmail.com>
2021-08-22 07:57:11 +0200

    1.0

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Redven Biker (XLife.fr)'
+description 'Discord : https://discord.gg/C3aN2bBVbB'
+version '1.0.0'
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'css/style.css',
+    'img/logo.png',
+    'music/music.mp3'
+}
